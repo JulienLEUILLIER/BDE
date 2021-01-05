fa419cf393598620721d25f41c6be535b4fb6e8c
JuYawn <julien.leuillier@gmail.com>
2021-01-05 15:04:24 +0100

    Changed Fake to Mock

--- TP8/TestsUnitaires/FakeMockTests.cs
+++ TP8/TestsUnitaires/FakeMockTests.cs
@@ -13,13 +13,13 @@ namespace TestsUnitaires
         public void TestStockedOrder()
         {
             // Testing the repository correctly stores the orders when adding to stock
-            IOrderingRepository repository = new FakeOrderingRepository();
-            Stock stock = new Stock(100m, repository);
+            var repoMock = Substitute.For<IOrderingRepository>();
+            Stock stock = new Stock(100m, repoMock);
             Order order = new Order(ProductGenerator.water, 5);
 
             stock.AddToStock(order);
 
-            Assert.True(repository.OrdersPassed.First().Equals(order));
+            repoMock.Received().SaveOrder(order);
         }
 
         [Fact]
